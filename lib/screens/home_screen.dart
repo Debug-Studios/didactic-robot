066629b0033ad275e7686bd9935f08d43a412669
Prajjwal <prajjwaldimri@hotmail.com>
2019-04-09 18:09:33 +0530

    Added a bottom nav to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hello")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.trending_up), title: Text('Trending')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), title: Text('Settings')),
+        ],
+      ),
     );
   }
 }
